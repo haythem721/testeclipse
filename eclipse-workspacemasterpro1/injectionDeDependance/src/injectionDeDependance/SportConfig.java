d70e77de37b82d1554ef9722a0a8a355939863f9
haythem <saoudi_haythem@yahoo.com>
2020-03-21 21:24:36 +0100

    inversion de controle avec code java sans fichier de configuration xml

---
+++ eclipse-workspacemasterpro1/injectionDeDependance/src/injectionDeDependance/SportConfig.java
@@ -0,0 +1,10 @@
+package injectionDeDependance;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("injectionDeDependance")
+public class SportConfig {
+
+}
--- eclipse-workspacemasterpro1/injectionDeDependance/src/injectionDeDependance/application.java
+++ eclipse-workspacemasterpro1/injectionDeDependance/src/injectionDeDependance/application.java
@@ -2,6 +2,7 @@ package injectionDeDependance;
 
 import java.util.Scanner;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class application {
@@ -16,7 +17,12 @@ public class application {
         Intermediaire i=new Intermediaire();
         i.setC(c);
         i.affiche();*/
-		ClassPathXmlApplicationContext contexte=new ClassPathXmlApplicationContext("applicationcontext.xml");
+		/*ClassPathXmlApplicationContext contexte=new ClassPathXmlApplicationContext("applicationcontext.xml");
+		Coach c=contexte.getBean("foot",Coach.class);
+		System.out.println(c.getCoach());
+		contexte.close();*/
+		
+		AnnotationConfigApplicationContext contexte=new AnnotationConfigApplicationContext(SportConfig.class);
 		Coach c=contexte.getBean("foot",Coach.class);
 		System.out.println(c.getCoach());
 		contexte.close();
